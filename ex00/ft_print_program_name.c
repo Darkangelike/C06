fbfb5a77af9e25846869b863cd8e6ff3d8c520ba
Darkangelike <72765580+Darkangelike@users.noreply.github.com>
2020-10-28 19:41:46 +0100

    initial

---
+++ ex00/ft_print_program_name.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itieu <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/28 16:32:37 by itieu             #+#    #+#             */
+/*   Updated: 2020/10/28 19:23:34 by itieu            ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_print_program_name(int argc, char **argv)
+{
+	ft_putstr(argv[0]);
+}
+
+int main (int argc, char **argv)
+{
+	ft_print_program_name(argc, argv);
+	return (0);
+}
+
